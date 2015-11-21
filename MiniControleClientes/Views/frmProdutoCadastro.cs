ab8ad6c1342b299fd1600111e00bb4344f764876
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-11-21 15:28:35 -0200

    Primeiro commit

---
+++ MiniControleClientes/Views/frmProdutoCadastro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniControleClientes.Views
+{
+    public partial class frmProdutoCadastro : Form
+    {
+        public frmProdutoCadastro()
+        {
+            InitializeComponent();
+        }
+    }
+}
